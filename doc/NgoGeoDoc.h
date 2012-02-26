da9b67ad262a54c8b5d54be619129e0da8b1cc82
Cedric <roman@numengo.com>
2012-02-26 14:08:43 +0100

    reorganize documentation

--- doc/NgoGeoDoc.h
+++ doc/NgoGeoDoc.h
@@ -1,60 +1,11 @@
-#ifndef _NgoGeoDoc_h_
-#define _NgoGeoDoc_h_
-
-/*******************************************************************************
-   FILE DESCRIPTION
-*******************************************************************************/
-/*!
-@file NgoGeoDoc.h
-@author Cedric ROMAN - roman@numengo.com
-@date May 2009
-@brief File containing all prototypes of geometry functions related to the Numengo Fluid Library
- */
-
 /*******************************************************************************
    LICENSE
 *******************************************************************************
  Copyright (C) 2009 Numengo (admin@numengo.com)
-
- This document is released under the terms of the numenGo EULA.  You should have received a
- copy of the numenGo EULA along with this file; see  the file LICENSE.TXT. If not, write at
- admin@numengo.com or at NUMENGO, 15 boulevard Vivier Merle, 69003 LYON - FRANCE
+ 
+ This document is released under the terms of the numenGo EULA.  You should have received a 
+ copy of the numenGo EULA along with this file; see  the file LICENSE.TXT. If not, write at 
+ admin@numengo.com or at NUMENGO, 15 boulevard Vivier Merle, 69003 LYON - FRANCE 
  You are not allowed to use, copy, modify or distribute this file unless you  conform to numenGo
  EULA license.
 */
-
-
-/*******************************************************************************
-   REVISIONS
-*******************************************************************************/
-
-/*******************************************************************************
-   INCLUDES
-*******************************************************************************/
-#include "ngogeo/NgoGeoExports.h"
-/*******************************************************************************
-   DEFINES / TYPDEFS / ENUMS
-*******************************************************************************/
- /*! @defgroup GroupNgoGeoWrapperC Geometry utilities
-This category gathers all functions related to geometry
- @ingroup GroupNgoFlUtils
- */
-
-/*! @defgroup GroupNgoGeoWrapperCSurf Surface utilities
-@ingroup GroupNgoGeoWrapperC
-@brief This section describes the class, its  current @ref GroupNgoGeoWrapperCSurfAvl "available descriptions".
-
-@defgroup GroupNgoGeoWrapperCSurfAvl   Available Descriptions
-@ingroup GroupNgoGeoWrapperC
-@brief This section escribes the derivated classes
-*/
-
-/*! @defgroup GroupNgoGeoWrapperCVol Volume utilities
-@ingroup GroupNgoGeoWrapperC
-@brief This section describes the class, its  current @ref GroupNgoGeoWrapperCVolAvl "available descriptions".
-
-@defgroup GroupNgoGeoWrapperCVolAvl   Available Descriptions
-@ingroup GroupNgoGeoWrapperC
-@brief This section escribes the derivated classes
-*/
-#endif
---
+++ doc/src/NgoDocModules.h
@@ -0,0 +1,25 @@
+/*******************************************************************************
+   GEO
+*******************************************************************************/
+/*! @defgroup GroupNgoGeoWrapperC Geometry utilities
+This category gathers all functions related to geometry
+ @ingroup GroupNgoFlUtils
+ */
+
+/*! @defgroup GroupNgoGeoWrapperCSurf Surface utilities
+@ingroup GroupNgoGeoWrapperC
+@brief This section describes the class, its  current @ref GroupNgoGeoWrapperCSurfAvl "available descriptions".
+
+@defgroup GroupNgoGeoWrapperCSurfAvl   Available Descriptions
+@ingroup GroupNgoGeoWrapperC
+@brief This section escribes the derivated classes
+*/
+
+/*! @defgroup GroupNgoGeoWrapperCVol Volume utilities
+@ingroup GroupNgoGeoWrapperC
+@brief This section describes the class, its  current @ref GroupNgoGeoWrapperCVolAvl "available descriptions".
+
+@defgroup GroupNgoGeoWrapperCVolAvl   Available Descriptions
+@ingroup GroupNgoGeoWrapperC
+@brief This section escribes the derivated classes
+*/
