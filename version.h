472537a2e13f4637a4dbf5d1db404ac6fed35e22
Cedric ROMAN <roman@numengo.com>
2013-12-06 11:56:57 +0100

    - update version
    Signed-off-by: Cedric ROMAN <roman@numengo.com>

--- version.h
+++ version.h
@@ -3,12 +3,12 @@
 
 #define RC_FILENAME "NgoGeo\0"
 #define RC_DESCRIPTION_STRING "geometry objects\0"
-#define RC_FILEVERSION 1,0,0,0
-#define RC_FILEVERSION_STRING "1.0rev0\0"
+#define RC_FILEVERSION 1,1,0,0
+#define RC_FILEVERSION_STRING "1.1rev0\0"
 
 #define RC_PRODUCTNAME_STRING "numenGo|Toolbox\0"
-#define RC_PRODUCTVERSION 12,02,0,0
-#define RC_PRODUCTVERSION_STRING "12.02rev0\0"
+#define RC_PRODUCTVERSION 13,12,1,0
+#define RC_PRODUCTVERSION_STRING "13.12rev1\0"
 
 #define RC_COMPANYNAME "NUMENGO\0"
 #define RC_COPYRIGHT_STRING "Copyright (C) 2012 Numengo\0"
